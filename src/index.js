94dd5d64832f0e9354504e0a07e45e7bd2e51729
Reiner Knudsen <reinerknudsen@me.com>
2023-11-14 18:25:35 +0100

    First step of completion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CardList from './CardList'
+import 'tachyons';
+import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <CardList robots={robots}/>
+    </div>
   </React.StrictMode>
 );
 
