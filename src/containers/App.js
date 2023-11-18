b021a9ef9d154415ed92381109196c4c7f8cd76c
Reiner Knudsen <reinerknudsen@me.com>
2023-11-18 18:29:50 +0100

    whoops 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -44,14 +44,14 @@ class App extends Component {
 				return robot.name.toLowerCase().includes(searchfield.toLowerCase())
 		})
 		return !robots.length ? 
-		<h1>Loading...</h1> : 
-				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange = {this.onSearchChange}/>
-					<Scroll>
-						<CardList robots = {filteredRobots} />
-					</Scroll>
-				</div>
+			<h1>Loading...</h1> : 
+			(<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange = {this.onSearchChange}/>
+				<Scroll>
+					<CardList robots = {filteredRobots} />
+				</Scroll>
+			</div>)
 	}
 }
 
